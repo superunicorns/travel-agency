8cfeb04e979d2a5e3f5f9eff3d0608f801624e01
UnicornDad8 <unicorndad8@gmail.com>
2025-04-29 16:10:53 -0300

    Agregando el componente Header al layout general

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,6 +1,14 @@
+import { Header } from "components";
+
 const AllUsers = () => {
   return (
-    <div>Users Table</div>
+    <main className="dashboard wrapper">
+      <Header 
+        title="Trips page"
+        description="Check out our current guests in real time"
+      />
+      All Users Page Content
+    </main>
   )
 }
 
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,6 +1,18 @@
+import { Header } from "components";
+
 const Dashboard = () => {
+  const user = {
+    name: "Adrian",
+  }
+
   return (
-    <div>Dashboard</div>
+    <main className="dashboard wrapper">
+      <Header 
+        title={`Welcome ${user?.name ?? "Guest"} 👋`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+      Dashboard Page Content
+    </main>
   )
 }
 
