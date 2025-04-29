739603129e65a5f1e0924660e7648b5af209aaec
UnicornDad8 <unicorndad8@gmail.com>
2025-04-29 17:23:56 -0300

    Terminar de implementar las StatsCard (tarea: hacer los charts funcionales con ayuda de chart js)

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -34,7 +34,7 @@ const Dashboard = () => {
             lastMonthCount={tripsCreated?.lastMonth}
           />
            <StatsCard 
-            headerTitle="Total Users"
+            headerTitle="Active Users"
             total={userRole.total}
             currentMonthCount={userRole?.currentMonth}
             lastMonthCount={userRole?.lastMonth}
