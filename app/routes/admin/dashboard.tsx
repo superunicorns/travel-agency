33cb9eb4bebf4c0b00f8f063418fac3de5b3acca
UnicornDad8 <unicorndad8@gmail.com>
2025-04-30 11:39:33 -0300

    Empezando primer garabato de TripCard en el componente Dashboard

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,16 +1,8 @@
 import { Header, StatsCard, TripCard } from "components";
+import { dashboardStats, user, allTrips } from "~/constants";
+
 
 const Dashboard = () => {
-  const user = {
-    name: "Adrian",
-  }
-  const dashboardStats = {
-    totalUsers: 12450,
-    usersJoined: { currentMonth: 218, lastMonth: 176 },
-    totalTrips: 3210,
-    tripsCreated: { currentMonth: 150, lastMonth: 250 },
-    userRole: { total: 62, currentMonth: 25, lastMonth: 15 }
-  }
   const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
 
   return (
@@ -41,7 +33,22 @@ const Dashboard = () => {
           />
         </div>
       </section>
-      <TripCard />
+      <section className="container">
+        <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+        <div className="trip-grid">
+          {allTrips.slice(0, 4).map(({ id, name, imageUrls, itinerary, tags, estimatedPrice }) => (
+            <TripCard 
+              key={id}
+              id={id.toString()}
+              name={name}
+              imageUrl={imageUrls[0]}
+              location={itinerary?.[0]?.location ?? ""}
+              tags={tags}
+              price={estimatedPrice}
+            />
+          ))}
+        </div>
+      </section>
     </main>
   )
 }
